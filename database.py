59dabe0457e738bf5c490069f591ed295d4055fc
areldai03 <125785005+areldai03@users.noreply.github.com>
2023-09-14 11:51:15 +0900

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    usermame = db.Column(db.String(30), nullable=False)
+    username = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
 
