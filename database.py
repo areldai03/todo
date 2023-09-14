1c0996dcff97f3249cb1f05aca65fb89395c72ee
areldai03 <125785005+areldai03@users.noreply.github.com>
2023-09-14 14:46:40 +0900

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.LargeBinary, nullable=False)
     salt = db.Column(db.LargeBinary, nullable=False)
 
 
