756cba7b63f58706e7f96143c8d07402b9007e61
areldai03 <dai.0127@icloud.com>
2023-09-14 14:12:05 +0900

    本番環境

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    salt = db.Column(db.String(255), nullable=False)
 
 
 class Task(db.Model):
