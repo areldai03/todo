69e29a39f9fac43e3d4b7fb38e5edd29eae93883
areldai03 <125785005+areldai03@users.noreply.github.com>
2023-09-14 12:10:26 +0900

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 
 class Task(db.Model):
