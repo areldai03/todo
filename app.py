a115c8f6682cdc89b0c9ebf73c04930d4e62838a
areldai03 <125785005+areldai03@users.noreply.github.com>
2023-09-14 14:01:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,8 @@ def login():
         username = request.form.get("userName")
         password = request.form.get('password')
         user = User.query.filter_by(username=username).first()
-        if user and user.username == username and bcrypt.checkpw(password.encode('utf-8'), user.password):
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), user.salt)
+        if user and user.username == username and hashed_password == user.password:
             session['user_id'] = user.id
             return redirect('/home')
         else:
